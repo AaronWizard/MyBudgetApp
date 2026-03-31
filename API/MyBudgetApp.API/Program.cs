ad7a5b3b4f18653f494592e7513892b6b54310a6
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-03-31 23:31:35 +0000

    API: Added Scalar and launch config

--- API/MyBudgetApp.API/Program.cs
+++ API/MyBudgetApp.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,9 +14,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
-
-if (!app.Environment.IsDevelopment())
+else
 {
     app.UseHttpsRedirection();
 }
