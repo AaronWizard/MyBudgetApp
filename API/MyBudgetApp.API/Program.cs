16f9a9a6bee4cdb0c1fa02cd2cb5baf51eaf0a4a
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-04-05 06:55:43 +0000

    DB context and JWT config

--- API/MyBudgetApp.API/Program.cs
+++ API/MyBudgetApp.API/Program.cs
@@ -1,8 +1,66 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MyBudgetApp.API.Data;
 using Scalar.AspNetCore;
+
+#region Helper Functions
+
+string GetConfigurationString(WebApplicationBuilder builder, string key)
+{
+    var result = builder.Configuration[key];
+    return result ?? throw new InvalidOperationException(
+        $"Missing configuration key: '{key}'"
+    );
+}
+
+string GetConnectionString(WebApplicationBuilder builder, string key)
+{
+    var result = builder.Configuration.GetConnectionString(key);
+    return result ?? throw new InvalidOperationException(
+        $"Missing connection string: '{key}'"
+    );
+}
+
+#endregion Helper Functions
+
+const string ValidIssuerKey = "Jwt:Issuer";
+const string ValidAudienceKey = "Jwt:Audience";
+const string SigningKeyKey = "Jwt:SigningKey";
+
+const string ConnectionStringKey = "MyBudgetApp";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var issuer = GetConfigurationString(builder, ValidIssuerKey);
+var audience = GetConfigurationString(builder, ValidAudienceKey);
+var signingKey = GetConfigurationString(builder, SigningKeyKey);
+
+var connectionString = GetConnectionString(builder, ConnectionStringKey);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(signingKey)
+            )
+        };
+    });
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(connectionString)
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,6 +79,7 @@ else
     app.UseHttpsRedirection();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
