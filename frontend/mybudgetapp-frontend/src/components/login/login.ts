b371913d6f57fe1dc51ed6c7b444ea3191f7575f
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-04-24 18:40:01 +0000

    Improved structure and styling of login and register forms

--- frontend/mybudgetapp-frontend/src/components/login/login.ts
+++ frontend/mybudgetapp-frontend/src/components/login/login.ts
@@ -3,10 +3,19 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
--- frontend/mybudgetapp-frontend/src/components/register/register.ts
+++ frontend/mybudgetapp-frontend/src/components/register/register.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
-import { MatFormField } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   imports: [
     ReactiveFormsModule,
-    MatCard,
-    MatCardHeader,
-    MatCardContent,
-    MatFormField,
+    MatCardModule,
+    MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
+    RouterLink,
   ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
