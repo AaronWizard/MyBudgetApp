7033a618cff3c08c2043242232c50712ff67f9f5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-04-24 18:02:49 +0000

    Cleaned up imports

--- frontend/mybudgetapp-frontend/src/components/login/login.ts
+++ frontend/mybudgetapp-frontend/src/components/login/login.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
-import { MatFormField } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    ReactiveFormsModule,
-    MatCard,
-    MatCardHeader,
-    MatCardContent,
-    MatFormField,
-    MatInputModule,
-  ],
+  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
