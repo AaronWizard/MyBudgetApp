ff1bcc2acf2cbd2a3557c2fce7de18d500cbc0c0
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-04-24 20:03:10 +0000

    Base API methods

--- frontend/mybudgetapp-frontend/src/services/api.ts
+++ frontend/mybudgetapp-frontend/src/services/api.ts
@@ -1,8 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
-  private url = 'http://localhost:5000/api/';
+  private readonly baseUrl = 'http://localhost:5000/api/';
+  private readonly versionParam = 'x-api-version';
+  private readonly version = '1.0';
+
+  private readonly baseHeaders = new HttpHeaders().set(this.versionParam, this.version);
+
+  constructor(private http: HttpClient) {}
+
+  get(method: string): Observable<Object> {
+    return this.http.get(this.baseUrl + method, { headers: this.baseHeaders });
+  }
+
+  post(method: string, requestBody: any | null): Observable<Object> {
+    return this.http.post(this.baseUrl + method, requestBody, { headers: this.baseHeaders });
+  }
+
+  put(method: string, requestBody: any | null): Observable<Object> {
+    return this.http.put(this.baseUrl + method, requestBody, { headers: this.baseHeaders });
+  }
+
+  delete(method: string, requestBody: any | null): Observable<Object> {
+    return this.http.delete(this.baseUrl + method, { headers: this.baseHeaders });
+  }
 }
