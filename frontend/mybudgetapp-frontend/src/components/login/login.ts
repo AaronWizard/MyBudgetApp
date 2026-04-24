324332f47e9b490b0b25acd2584380cac71f7b00
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-04-24 16:40:25 +0000

    Angular Material

--- frontend/mybudgetapp-frontend/src/components/login/login.ts
+++ frontend/mybudgetapp-frontend/src/components/login/login.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatCard, MatCardHeader, MatCardContent, MatFormField, MatInputModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
--- frontend/mybudgetapp-frontend/src/components/register/register.ts
+++ frontend/mybudgetapp-frontend/src/components/register/register.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatCard, MatCardHeader, MatCardContent, MatFormField, MatInputModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
